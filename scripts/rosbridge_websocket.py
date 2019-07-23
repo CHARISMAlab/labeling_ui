f362f5fef5adb998b36e087f8dcb876efd84fddb
CHARISMA Lab <charisma@engr.oregonstate.edu>
2019-07-23 13:47:57 -0700

    Samar: Bringing Abrar's code to git

---
+++ scripts/rosbridge_websocket.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import roslib
+import rospy
+
+from geometry_msgs.msg import Twist
+
+import sys, select, termios, tty
+
+from std_msgs.msg import String
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Vector3
+
+import time
+from std_msgs.msg import Int8
+from std_msgs.msg import UInt16
+from sensor_msgs.msg import Joy
+
+msg = """
+**Connection successful**
+Now you are reading from both UI and Publishing to the neato!
+---------------------------
+use the interface to start laabeling your data:
+"""
+
+
+def listener():
+	rospy.spin()
+
+if __name__ == '__main__':
+
+	# rospy.init_node('switch', anonymous=True)
+	# pubon01 = rospy.Publisher("/cbon01", Int8, queue_size=10)
+	# pubon02 = rospy.Publisher("/cbon02", Int8, queue_size=10)
+	# pubon03 = rospy.Publisher("/cbon03", Int8, queue_size=10)
+	# pubon04 = rospy.Publisher("/cbon04", Int8, queue_size=10)
+	try:
+		print msg
+		listener()
+	except rospy.ROSInterruptException:
+		pass
